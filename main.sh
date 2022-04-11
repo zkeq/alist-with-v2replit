568e4f76011e28c98113b8dec7935ff51953f064
valetzx <45054400+valetzx@users.noreply.github.com>
2022-04-11 09:39:34 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,4 +8,5 @@ if [ ! -d "~/nginx" ];then
 fi
 
 v2ray/sbin/v2ray -config v2ray/etc/config.json >/dev/null 2>&1 &
-/home/runner/nginx/sbin/nginx -g 'daemon off;'
+/home/runner/nginx/sbin/nginx -g 'daemon off;' & 
+./alist echo #启动alist并让终端输出
