9a2c726a638c65b7a9e9dab3d6c193f0af1d0200
valetzx <45054400+valetzx@users.noreply.github.com>
2022-04-11 08:36:22 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 chmod +x alist
-chmod +x v2ray
+
+if [ ! -d "~/nginx" ];then
+	\cp -ax .nginx ~/nginx
+fi
+
 v2ray/sbin/v2ray -config v2ray/etc/config.json >/dev/null 2>&1 &
 /home/runner/nginx/sbin/nginx -g 'daemon off;'
